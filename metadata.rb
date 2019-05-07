f32dbdbda9a8934261767feb17678bee6354ea4e
Ashley Abbott <ashley.abbott@eagleeye.com>
2019-05-07 11:58:27 +0100

    tweaks

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/sous-chefs/gpg'
 issues_url       'https://github.com/sous-chefs/gpg/issues'
 version          '1.0.0'
-chef_version     '>= 13'
+chef_version     '>= 12'
 
 depends 'yum-epel'
 
